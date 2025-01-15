8dd5a3b01a4a38a3461466e0117c0cceb8a0c0aa
Darcey Pollard <darcey@Darceys-Work-Mac.local>
2025-01-16 08:16:32 +1100

    add report

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
     count_words_in_book = count_words(file_contents)
-    print(count_words_in_book)
+    count_letters_in_book = count_letters(file_contents)
+
+    print("--- Begin report of books/frankensein.txt ---")
+    print(f"{count_words_in_book} words found in the document")
+    print("")
+    create_report(count_letters_in_book)
+    print("--- End report ---")
 
 def count_words(text):
     return len(text.split())
+
+def count_letters(text):
+    count={}
+    lowered = text.lower()
+    for c in lowered:
+        count[c] = count.get(c, 0) + 1
+    return count
+
+def sort_on(dict):
+    return dict["num"]
+
+def create_report(letters):
+    report_list = []
+    for letter in letters:
+        if letter.isalpha():
+            report_list.append({"letter": letter, "num": letters[letter]})
+    report_list.sort(key=sort_on, reverse=True)
+
+    for letter in report_list:
+        print(f"The {letter['letter']} character was found {letter['num']} times")
 
 main()
